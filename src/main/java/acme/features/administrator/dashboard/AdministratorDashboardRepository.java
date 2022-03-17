9424efd76b9b7457624e138843d93e8c76e2f800
Daniel Rodriguez Camacho <danrodcam@alum.us.es>
2022-03-17 12:28:37 +0100

    Task 010:Done

--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
@@ -7,11 +7,61 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface AdministratorDashboardRepository extends AbstractRepository {
 
-	@Query("select count(*) from Components")
+	@Query("select count(e) from Items e where e.type==Component")
 	Double numberOfComponents();
-
-	@Query("select count(*) from Tools")
+	
+	@Query("select avg(e.retailPrice) from Items e where e.type==Component group by e.technology")
+	Double averageComponents();
+	
+	@Query("select stddev(e.retailPrice) from Items e where e.type==Component group by e.technology")
+	Double desviationComponents();
+	
+	@Query("select max(e.retailPrice) from Items e where e.type==Component group by e.technology")
+	Double maxComponents();
+	
+	@Query("select min(e.retailPrice) from Items e where e.type==Component group by e.technology")
+	Double minComponents();
+	
+	
+	
+	
+	@Query("select count(e) from Tools e where e.type==Tool")
 	Double numberOfTools();
+	
+	@Query("select avg(e.retailPrice) from Items e where e.type==Tool")
+	Double averageTools();
+	
+	@Query("select stddev(e.retailPrice) from Items e where e.type==Tool")
+	Double desviationTools();
+	
+	@Query("select max(e.retailPrice) from Items e where e.type==Tool")
+	Double maxTools();
+	
+	@Query("select min(e.retailPrice) from Items e where e.type==Tool")
+	Double minTools();
+	
+	
+	
+	
+	@Query("select count(e) from Patronage e group by e.status ")
+	Double numberOfPatronages();
+	
+	@Query("select avg(e.budget) from Patronage e group by e.status")
+	Double averagePatronages();
+	
+	@Query("select stddev(e.budget) from Patronage e group by e.status")
+	Double desviationPatronages();
+	
+	@Query("select max(e.budget) from Patronage e group by e.status")
+	Double maxPatronages();
+	
+	@Query("select min(e.budget) from Patronage e group by e.status")
+	Double minPatronages();
+	
+	
+	
+	
+	
 
 	
 }
