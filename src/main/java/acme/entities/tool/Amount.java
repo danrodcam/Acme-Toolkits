78dfb006f0bf0535800416f8457db3a140204882
antrosbarDP <antrosbar@alum.us.es>
2022-03-17 17:56:24 +0100

    Task 016 - Item entity

--- src/main/java/acme/entities/tool/Amount.java
+++ src/main/java/acme/entities/tool/Amount.java
@@ -1,16 +1,10 @@
 package acme.entities.tool;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.PositiveOrZero;
-
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
 
 import acme.entities.tookit.Toolkit;
 import acme.framework.entities.AbstractEntity;
@@ -20,7 +14,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Tool extends AbstractEntity {
+public class Amount extends AbstractEntity {
 	
 	// Serialisation identifier -----------------------------------------------
 
@@ -29,25 +23,8 @@ public class Tool extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	protected String 				name;
+	protected int				units;
 	
-	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
-	@Column(unique=true)
-	protected String 				code;
-	
-	@NotBlank
-	@Length (max = 100)
-	protected String 				technology;
-	
-	@NotBlank
-	@Length (max = 255)
-	protected String 				description;
-	
-	@PositiveOrZero
-	protected double 				retailPrice;
-	
-	@URL
-	protected String 				link;
 	
 	// Derived attributes -----------------------------------------------------
 	
@@ -55,7 +32,7 @@ public class Tool extends AbstractEntity {
 	
 	@NotNull
 	@Valid
-	@ManyToOne(optional = true)
+	@ManyToOne(optional = false)
 	protected Toolkit 				toolkit;
 
 }
