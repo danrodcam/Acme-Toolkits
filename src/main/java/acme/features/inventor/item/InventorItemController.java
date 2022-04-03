b5c725dc4d9bd28360af49d5b01bfbf208a41c67
antrosbarDP <antrosbar@alum.us.es>
2022-04-03 13:57:02 +0200

    Minor fix

--- src/main/java/acme/features/inventor/item/InventorItemController.java
+++ src/main/java/acme/features/inventor/item/InventorItemController.java
@@ -22,7 +22,7 @@ import acme.framework.controllers.AbstractController;
 import acme.roles.Inventor;
 
 @Controller
-public class InventorComponentController extends AbstractController<Inventor, Item> {
+public class InventorItemController extends AbstractController<Inventor, Item> {
 
 	// Internal state ---------------------------------------------------------
 
